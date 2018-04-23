cb017dcfd46f7114098ebe7c1a23dc9f1b44f289
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GridCommander/CommanderTab.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace GridCommander
+{
+    partial class CommanderTab
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject1 = new DevExpress.Utils.SerializableAppearanceObject();
+            this.te_CurentText = new DevExpress.XtraEditors.TextEdit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmb_Drives = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.Grid = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colAttributes = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemButtonEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit();
+            this.colFullName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colExtension = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCreationTime = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSize = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemPictureEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.te_CurentText.Properties)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cmb_Drives.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemButtonEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // te_CurentText
+            // 
+            this.te_CurentText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.te_CurentText.Location = new System.Drawing.Point(0, 20);
+            this.te_CurentText.Name = "te_CurentText";
+            this.te_CurentText.Properties.ReadOnly = true;
+            this.te_CurentText.Size = new System.Drawing.Size(775, 20);
+            this.te_CurentText.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmb_Drives);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 20);
+            this.panel1.TabIndex = 5;
+            // 
+            // cmb_Drives
+            // 
+            this.cmb_Drives.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cmb_Drives.Location = new System.Drawing.Point(0, 0);
+            this.cmb_Drives.Name = "cmb_Drives";
+            this.cmb_Drives.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmb_Drives.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
+            this.cmb_Drives.Size = new System.Drawing.Size(42, 20);
+            this.cmb_Drives.TabIndex = 1;
+            this.cmb_Drives.EditValueChanged += new System.EventHandler(this.cmb_Drives_EditValueChanged);
+            this.cmb_Drives.EditValueChanging += new DevExpress.XtraEditors.Controls.ChangingEventHandler(this.cmb_Drives_EditValueChanging);
+            // 
+            // Grid
+            // 
+            this.Grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Grid.Location = new System.Drawing.Point(0, 40);
+            this.Grid.MainView = this.gridView1;
+            this.Grid.Name = "Grid";
+            this.Grid.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemButtonEdit1,
+            this.repositoryItemPictureEdit1});
+            this.Grid.Size = new System.Drawing.Size(775, 445);
+            this.Grid.TabIndex = 1;
+            this.Grid.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.Grid.Load += new System.EventHandler(this.Grid_Load);
+            this.Grid.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CommanderTab_KeyDown);
+            // 
+            // gridView1
+            // 
+            this.gridView1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colAttributes,
+            this.colName,
+            this.colFullName,
+            this.colExtension,
+            this.colCreationTime,
+            this.colSize});
+            this.gridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView1.GridControl = this.Grid;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsBehavior.ReadOnly = true;
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView1.OptionsSelection.MultiSelect = true;
+            this.gridView1.OptionsView.ShowHorzLines = false;
+            this.gridView1.OptionsView.ShowVertLines = false;
+            this.gridView1.CustomColumnDisplayText += new DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventHandler(this.gridView1_CustomColumnDisplayText);
+            this.gridView1.CustomDrawCell += new DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventHandler(this.gridView1_CustomDrawCell);
+            this.gridView1.DoubleClick += new System.EventHandler(this.gridView1_DoubleClick);
+            // 
+            // colAttributes
+            // 
+            this.colAttributes.Caption = "Attributes";
+            this.colAttributes.FieldName = "Attributes";
+            this.colAttributes.Name = "colAttributes";
+            this.colAttributes.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
+            // 
+            // colName
+            // 
+            this.colName.Caption = "Name";
+            this.colName.ColumnEdit = this.repositoryItemButtonEdit1;
+            this.colName.FieldName = "Name";
+            this.colName.Name = "colName";
+            this.colName.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways;
+            this.colName.Visible = true;
+            this.colName.VisibleIndex = 0;
+            // 
+            // repositoryItemButtonEdit1
+            // 
+            this.repositoryItemButtonEdit1.AutoHeight = false;
+            this.repositoryItemButtonEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph, "", 0, false, true, true, DevExpress.XtraEditors.ImageLocation.MiddleLeft, null, new DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, "", null, null, true)});
+            this.repositoryItemButtonEdit1.Name = "repositoryItemButtonEdit1";
+            this.repositoryItemButtonEdit1.ReadOnly = true;
+            // 
+            // colFullName
+            // 
+            this.colFullName.Caption = "FullName";
+            this.colFullName.FieldName = "FullName";
+            this.colFullName.Name = "colFullName";
+            // 
+            // colExtension
+            // 
+            this.colExtension.Caption = "Extension";
+            this.colExtension.FieldName = "Extension";
+            this.colExtension.Name = "colExtension";
+            this.colExtension.SortMode = DevExpress.XtraGrid.ColumnSortMode.Custom;
+            this.colExtension.Visible = true;
+            this.colExtension.VisibleIndex = 1;
+            // 
+            // colCreationTime
+            // 
+            this.colCreationTime.Caption = "Date";
+            this.colCreationTime.FieldName = "CreationTime";
+            this.colCreationTime.Name = "colCreationTime";
+            this.colCreationTime.Visible = true;
+            this.colCreationTime.VisibleIndex = 2;
+            // 
+            // colSize
+            // 
+            this.colSize.Caption = "Size";
+            this.colSize.Name = "colSize";
+            this.colSize.Visible = true;
+            this.colSize.VisibleIndex = 3;
+            // 
+            // repositoryItemPictureEdit1
+            // 
+            this.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1";
+            // 
+            // CommanderTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.te_CurentText);
+            this.Controls.Add(this.panel1);
+            this.Name = "CommanderTab";
+            this.Size = new System.Drawing.Size(775, 485);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CommanderTab_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.te_CurentText.Properties)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cmb_Drives.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemButtonEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemPictureEdit1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraGrid.GridControl Grid;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.ComboBoxEdit cmb_Drives;
+        private DevExpress.XtraGrid.Columns.GridColumn colAttributes;
+        private DevExpress.XtraGrid.Columns.GridColumn colName;
+        private DevExpress.XtraGrid.Columns.GridColumn colFullName;
+        private DevExpress.XtraGrid.Columns.GridColumn colExtension;
+        private DevExpress.XtraGrid.Columns.GridColumn colCreationTime;
+        private DevExpress.XtraEditors.TextEdit te_CurentText;
+        private DevExpress.XtraGrid.Columns.GridColumn colSize;
+        private DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit repositoryItemButtonEdit1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit repositoryItemPictureEdit1;
+    }
+}
---
+++ VB/GridCommander/CommanderTab.Designer.vb
@@ -0,0 +1,206 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace GridCommander
+	Partial Public Class CommanderTab
+		''' <summary> 
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Component Designer generated code"
+
+		''' <summary> 
+		''' Required method for Designer support - do not modify 
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim serializableAppearanceObject1 As New DevExpress.Utils.SerializableAppearanceObject()
+			Me.te_CurentText = New DevExpress.XtraEditors.TextEdit()
+			Me.panel1 = New System.Windows.Forms.Panel()
+			Me.cmb_Drives = New DevExpress.XtraEditors.ComboBoxEdit()
+			Me.Grid = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.colAttributes = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.colName = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.repositoryItemButtonEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit()
+			Me.colFullName = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.colExtension = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.colCreationTime = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.colSize = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.repositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+			CType(Me.te_CurentText.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.panel1.SuspendLayout()
+			CType(Me.cmb_Drives.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.Grid, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.repositoryItemButtonEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' te_CurentText
+			' 
+			Me.te_CurentText.Dock = System.Windows.Forms.DockStyle.Top
+			Me.te_CurentText.Location = New System.Drawing.Point(0, 20)
+			Me.te_CurentText.Name = "te_CurentText"
+			Me.te_CurentText.Properties.ReadOnly = True
+			Me.te_CurentText.Size = New System.Drawing.Size(775, 20)
+			Me.te_CurentText.TabIndex = 6
+			' 
+			' panel1
+			' 
+			Me.panel1.Controls.Add(Me.cmb_Drives)
+			Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
+			Me.panel1.Location = New System.Drawing.Point(0, 0)
+			Me.panel1.Name = "panel1"
+			Me.panel1.Size = New System.Drawing.Size(775, 20)
+			Me.panel1.TabIndex = 5
+			' 
+			' cmb_Drives
+			' 
+			Me.cmb_Drives.Dock = System.Windows.Forms.DockStyle.Left
+			Me.cmb_Drives.Location = New System.Drawing.Point(0, 0)
+			Me.cmb_Drives.Name = "cmb_Drives"
+			Me.cmb_Drives.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.cmb_Drives.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor
+			Me.cmb_Drives.Size = New System.Drawing.Size(42, 20)
+			Me.cmb_Drives.TabIndex = 1
+'			Me.cmb_Drives.EditValueChanged += New System.EventHandler(Me.cmb_Drives_EditValueChanged);
+'			Me.cmb_Drives.EditValueChanging += New DevExpress.XtraEditors.Controls.ChangingEventHandler(Me.cmb_Drives_EditValueChanging);
+			' 
+			' Grid
+			' 
+			Me.Grid.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.Grid.Location = New System.Drawing.Point(0, 40)
+			Me.Grid.MainView = Me.gridView1
+			Me.Grid.Name = "Grid"
+			Me.Grid.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() { Me.repositoryItemButtonEdit1, Me.repositoryItemPictureEdit1})
+			Me.Grid.Size = New System.Drawing.Size(775, 445)
+			Me.Grid.TabIndex = 1
+			Me.Grid.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+'			Me.Grid.Load += New System.EventHandler(Me.Grid_Load);
+'			Me.Grid.KeyDown += New System.Windows.Forms.KeyEventHandler(Me.CommanderTab_KeyDown);
+			' 
+			' gridView1
+			' 
+			Me.gridView1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colAttributes, Me.colName, Me.colFullName, Me.colExtension, Me.colCreationTime, Me.colSize})
+			Me.gridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+			Me.gridView1.GridControl = Me.Grid
+			Me.gridView1.Name = "gridView1"
+			Me.gridView1.OptionsBehavior.Editable = False
+			Me.gridView1.OptionsBehavior.ReadOnly = True
+			Me.gridView1.OptionsSelection.EnableAppearanceFocusedCell = False
+			Me.gridView1.OptionsSelection.MultiSelect = True
+			Me.gridView1.OptionsView.ShowHorzLines = False
+			Me.gridView1.OptionsView.ShowVertLines = False
+'			Me.gridView1.CustomColumnDisplayText += New DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventHandler(Me.gridView1_CustomColumnDisplayText);
+'			Me.gridView1.CustomDrawCell += New DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventHandler(Me.gridView1_CustomDrawCell);
+'			Me.gridView1.DoubleClick += New System.EventHandler(Me.gridView1_DoubleClick);
+			' 
+			' colAttributes
+			' 
+			Me.colAttributes.Caption = "Attributes"
+			Me.colAttributes.FieldName = "Attributes"
+			Me.colAttributes.Name = "colAttributes"
+			Me.colAttributes.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways
+			' 
+			' colName
+			' 
+			Me.colName.Caption = "Name"
+			Me.colName.ColumnEdit = Me.repositoryItemButtonEdit1
+			Me.colName.FieldName = "Name"
+			Me.colName.Name = "colName"
+			Me.colName.ShowButtonMode = DevExpress.XtraGrid.Views.Base.ShowButtonModeEnum.ShowAlways
+			Me.colName.Visible = True
+			Me.colName.VisibleIndex = 0
+			' 
+			' repositoryItemButtonEdit1
+			' 
+			Me.repositoryItemButtonEdit1.AutoHeight = False
+			Me.repositoryItemButtonEdit1.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph, "", 0, False, True, True, DevExpress.XtraEditors.ImageLocation.MiddleLeft, Nothing, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, "", Nothing, Nothing, True)})
+			Me.repositoryItemButtonEdit1.Name = "repositoryItemButtonEdit1"
+			Me.repositoryItemButtonEdit1.ReadOnly = True
+			' 
+			' colFullName
+			' 
+			Me.colFullName.Caption = "FullName"
+			Me.colFullName.FieldName = "FullName"
+			Me.colFullName.Name = "colFullName"
+			' 
+			' colExtension
+			' 
+			Me.colExtension.Caption = "Extension"
+			Me.colExtension.FieldName = "Extension"
+			Me.colExtension.Name = "colExtension"
+			Me.colExtension.SortMode = DevExpress.XtraGrid.ColumnSortMode.Custom
+			Me.colExtension.Visible = True
+			Me.colExtension.VisibleIndex = 1
+			' 
+			' colCreationTime
+			' 
+			Me.colCreationTime.Caption = "Date"
+			Me.colCreationTime.FieldName = "CreationTime"
+			Me.colCreationTime.Name = "colCreationTime"
+			Me.colCreationTime.Visible = True
+			Me.colCreationTime.VisibleIndex = 2
+			' 
+			' colSize
+			' 
+			Me.colSize.Caption = "Size"
+			Me.colSize.Name = "colSize"
+			Me.colSize.Visible = True
+			Me.colSize.VisibleIndex = 3
+			' 
+			' repositoryItemPictureEdit1
+			' 
+			Me.repositoryItemPictureEdit1.Name = "repositoryItemPictureEdit1"
+			' 
+			' CommanderTab
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.Controls.Add(Me.Grid)
+			Me.Controls.Add(Me.te_CurentText)
+			Me.Controls.Add(Me.panel1)
+			Me.Name = "CommanderTab"
+			Me.Size = New System.Drawing.Size(775, 485)
+'			Me.KeyDown += New System.Windows.Forms.KeyEventHandler(Me.CommanderTab_KeyDown);
+			CType(Me.te_CurentText.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.panel1.ResumeLayout(False)
+			CType(Me.cmb_Drives.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.Grid, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.repositoryItemButtonEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.repositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private panel1 As System.Windows.Forms.Panel
+		Private WithEvents Grid As DevExpress.XtraGrid.GridControl
+		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private WithEvents cmb_Drives As DevExpress.XtraEditors.ComboBoxEdit
+		Private colAttributes As DevExpress.XtraGrid.Columns.GridColumn
+		Private colName As DevExpress.XtraGrid.Columns.GridColumn
+		Private colFullName As DevExpress.XtraGrid.Columns.GridColumn
+		Private colExtension As DevExpress.XtraGrid.Columns.GridColumn
+		Private colCreationTime As DevExpress.XtraGrid.Columns.GridColumn
+		Private te_CurentText As DevExpress.XtraEditors.TextEdit
+		Private colSize As DevExpress.XtraGrid.Columns.GridColumn
+		Private repositoryItemButtonEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemButtonEdit
+		Private repositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+	End Class
+End Namespace
