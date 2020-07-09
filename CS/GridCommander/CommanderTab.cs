3f2297a062c1213a981c535bb0ab06bf4365bcbb
SvetlanaPN <svetlana.peksheva@devexpress.com>
2020-07-09 20:11:53 +0300

    cache icons and remove using row handles in CustomColumnDisplayText

--- CS/GridCommander/CommanderTab.cs
+++ CS/GridCommander/CommanderTab.cs
@@ -10,129 +10,108 @@ using System.Windows.Forms;
 
 using DevExpress.XtraEditors;
 using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid.Views.Grid;
 
-namespace GridCommander
-{
-    public partial class CommanderTab : DevExpress.XtraEditors.XtraUserControl
-    {
+namespace GridCommander {
+    public partial class CommanderTab : DevExpress.XtraEditors.XtraUserControl {
         DirectoryInfo _parent;
         DirectoryInfo _curentDirectory;
-        DirectoryInfo CurentDirectory
-        {
+        DirectoryInfo CurentDirectory {
             get { return _curentDirectory; }
-            set
-            {
-                if (_curentDirectory == value)
+            set {
+                if(_curentDirectory == value)
                     return;
                 te_CurentText.Text = value.FullName;
                 _curentDirectory = value;
                 List<FileSystemInfo> infos = new List<FileSystemInfo>();
                 infos.AddRange(_curentDirectory.GetFileSystemInfos());
-                if (_curentDirectory.Parent != null)
-                {
+                if(_curentDirectory.Parent != null) {
                     _parent = _curentDirectory.Parent;
                     infos.Insert(0, _parent);
                 }
                 Grid.DataSource = infos;
             }
         }
-        public CommanderTab()
-        {
+        Icon directoryIcon;
+        Dictionary<string, Icon> icons;
+        public CommanderTab() {
             InitializeComponent();
+            directoryIcon = new Icon("../../Resources/Folder-Open.ico");
+            icons = new Dictionary<string, Icon>();
         }
 
-        private void Grid_Load(object sender, EventArgs e)
-        {
+        private void Grid_Load(object sender, EventArgs e) {
             cmb_Drives.Properties.Items.AddRange(Directory.GetLogicalDrives());
             cmb_Drives.Text = cmb_Drives.Properties.Items[0].ToString();
         }
 
-        private void cmb_Drives_EditValueChanged(object sender, EventArgs e)
-        {
+        private void cmb_Drives_EditValueChanged(object sender, EventArgs e) {
             CurentDirectory = new DirectoryInfo(cmb_Drives.Text);
         }
 
-        private void cmb_Drives_EditValueChanging(object sender, DevExpress.XtraEditors.Controls.ChangingEventArgs e)
-        {
-            try 
-            {
+        private void cmb_Drives_EditValueChanging(object sender, DevExpress.XtraEditors.Controls.ChangingEventArgs e) {
+            try {
                 CurentDirectory = new DirectoryInfo(e.NewValue.ToString());
             }
-            catch
-            {
+            catch {
                 CurentDirectory = new DirectoryInfo(e.OldValue.ToString());
                 e.Cancel = true;
             }
         }
 
-        private void gridView1_DoubleClick(object sender, EventArgs e)
-        {
-            FileSystemInfo fi = gridView1.GetRow(gridView1.FocusedRowHandle) as FileSystemInfo;
-            if ((fi.Attributes & FileAttributes.Directory) != FileAttributes.Directory)
-            {
-                Process p = Process.Start(fi.FullName);
-            }
+        private void gridView1_DoubleClick(object sender, EventArgs e) {
+            GridView view = sender as GridView;
+            FileSystemInfo fi = view.GetRow(view.FocusedRowHandle) as FileSystemInfo;
+            if((fi.Attributes & FileAttributes.Directory) != FileAttributes.Directory)
+                Process.Start(fi.FullName);
             else
-            {
                 CurentDirectory = new DirectoryInfo(fi.FullName);
-            }
-            return;
         }
 
-        private void CommanderTab_KeyDown(object sender, KeyEventArgs e)
-        {
-            if ((e.KeyCode == Keys.Back)&&(CurentDirectory.Parent!=null))
-            {
+        private void CommanderTab_KeyDown(object sender, KeyEventArgs e) {
+            if(e.KeyCode == Keys.Back && CurentDirectory.Parent != null)
                 CurentDirectory = CurentDirectory.Parent;
-            }
         }
 
-        private void gridView1_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e)
-        {
-            if (e.Column == colName)
-            {
+        private void gridView1_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e) {
+            GridView view = sender as GridView;
+            if(e.Column == colName) {
                 Icon ic;
-                FileAttributes fa = (FileAttributes)gridView1.GetRowCellValue(e.RowHandle, colAttributes);
-                if ((fa & FileAttributes.Directory) == FileAttributes.Directory)
-                {
-                    ic = new Icon("../../Resources/Folder-Open.ico");
-                }
-                else
-                {
-                    string path = (string)gridView1.GetRowCellValue(e.RowHandle, colFullName);
-                    ic = Icon.ExtractAssociatedIcon(path);
+                FileAttributes fa = (FileAttributes)view.GetRowCellValue(e.RowHandle, colAttributes);
+                if((fa & FileAttributes.Directory) == FileAttributes.Directory) 
+                    ic = directoryIcon;
+                else { 
+                    string path = (string)view.GetRowCellValue(e.RowHandle, colFullName);
+                    if(!icons.ContainsKey(path))
+                        icons.Add(path, Icon.ExtractAssociatedIcon(path));
+                    ic = icons[path];
                 }
                 Rectangle rec = e.Bounds;
                 rec.Width = rec.Height;
                 rec.Inflate(-2, -2);
                 rec.Offset(1, 1);
-                e.Cache.Graphics.DrawIcon(ic, rec);
+                e.Graphics.DrawIcon(ic, rec);
             }
         }
 
-        private void gridView1_CustomColumnDisplayText(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs e)
-        {
-            int rowHandler = gridView1.GetRowHandle(e.ListSourceRowIndex);
-            if (e.Column == colName)
-            {
-                string s = (string)gridView1.GetRowCellValue(rowHandler, colFullName);
-                if ((_parent != null) && (s == _parent.FullName))
+        private void gridView1_CustomColumnDisplayText(object sender, DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs e) {
+            GridView view = sender as GridView;
+            if(e.Column == colName) {
+                string s = (string)view.GetListSourceRowCellValue(e.ListSourceRowIndex, colFullName);
+                if((_parent != null) && (s == _parent.FullName))
                     e.DisplayText = "[..]";
-                else
-                {
-                    FileAttributes fa = (FileAttributes)gridView1.GetRowCellValue(rowHandler, colAttributes);
-                    if ((fa & FileAttributes.Directory) == FileAttributes.Directory)
+                else {
+                    FileAttributes fa = (FileAttributes)view.GetListSourceRowCellValue(e.ListSourceRowIndex, colAttributes);
+                    if((fa & FileAttributes.Directory) == FileAttributes.Directory)
                         e.DisplayText = "[" + e.Value + "]";
                 }
             }
-            if (e.Column == colSize)
-            {
-                FileAttributes fa = (FileAttributes)gridView1.GetRowCellValue(rowHandler, colAttributes);
-                if ((fa & FileAttributes.Directory) == FileAttributes.Directory)
+            if(e.Column == colSize) {
+                FileAttributes fa = (FileAttributes)view.GetListSourceRowCellValue(e.ListSourceRowIndex, colAttributes);
+                if((fa & FileAttributes.Directory) == FileAttributes.Directory)
                     e.DisplayText = "<DIR>";
-                else
-                {
-                    string path = (string)gridView1.GetRowCellValue(rowHandler, colFullName);
+                else {
+                    string path = (string)view.GetListSourceRowCellValue(e.ListSourceRowIndex, colFullName);
                     FileInfo fi = new FileInfo(path);
                     e.DisplayText = fi.Length.ToString();
                 }
